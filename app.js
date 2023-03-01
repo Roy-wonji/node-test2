e2efbf60b7252c7cbd00208003fd8d8d4a0e00e7
Roy-wonji <shuwj81@daum.net>
2023-03-02 00:12:14 +0900

    수정

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ const router = jsonServer.router('cream.json')
 var app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 app.use(router)
 
 const customRouter = jsonServer.rewriter({
@@ -31,8 +31,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
